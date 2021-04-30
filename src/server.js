83e737984172d9081350c468664e797a4e55cd7d
pooh <cktjdgus45@naver.com>
2021-05-01 08:29:31 +0900

    add morgan middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan('dev');
 
-const handleListening = (req, res) => {
+const handleListening = () => {
     console.log(`Listening on PORT:${PORT}`);
 }
 
 app.listen(4000, handleListening);
+
+const handleHome = (req, res) => {
+    res.send('response from server');
+}
+
+app.use(logger);
+app.get('/', handleHome);
