930d8650b9866ee8f1c8f434c72bf6c2a1a772f8
pooh <cktjdgus45@naver.com>
2021-05-06 19:23:07 +0900

    view engine DIR setting

--- src/server.js
+++ src/server.js
@@ -15,8 +15,9 @@ const handleListening = () => {
 
 app.listen(4000, handleListening);
 
+app.set('view engine', 'pug');
+app.set('views', process.cwd() + '/src/views');
 app.use(logger);
-
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
