1375322f5b5b19efd430df489f4b67fb3fd6df77
pooh <cktjdgus45@naver.com>
2021-05-06 20:27:10 +0900

    Working View Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,9 +3,34 @@ const fakeUser = {
     loggedIn: true
 }
 
-
 export const home = (req, res) => {
-    res.render('home', { pageTitle: "home", potato: "potato", fakeUser });
+    const videos = [
+        {
+            title: "video1",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1
+        },
+        {
+            title: "video2",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 2
+        },
+        {
+            title: "video3",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 3
+        },
+    ];
+    res.render('home', { pageTitle: "home", potato: "potato", fakeUser, videos });
 }
 export const search = (req, res) => {
     res.send('search', { pageTitle: "search" });
