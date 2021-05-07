237ff8ab14f472e8075960592a4326962605a772
pooh <cktjdgus45@naver.com>
2021-05-07 18:04:34 +0900

    handle upload process with fake db

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,15 +1,14 @@
 import express from 'express';
-import { deleteVideo, getEdit, postEdit, upload, watch } from '../controllers/videoController';
+import { deleteVideo, getEdit, getUpload, postEdit, postUpload, watch } from '../controllers/videoController';
 
 const videoRouter = express.Router();
 
 
 
-videoRouter.get('/upload', upload);
 videoRouter.get('/:id(\\d+)', watch);
-
+videoRouter.get('/:id(\\d+)/delete', deleteVideo);
+videoRouter.route('/upload').get(getUpload).post(postUpload);
 videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit);
 
-videoRouter.get('/:id(\\d+)/delete', deleteVideo);
 
 export default videoRouter;
