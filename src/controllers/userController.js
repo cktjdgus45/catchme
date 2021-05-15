a1e348f7680b0eb51e09e446491eb79d70d06596
pooh <cktjdgus45@naver.com>
2021-05-15 22:32:56 +0900

    handle avoid repeat account process with

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,9 +7,16 @@ export const getJoin = (req, res) => {
     return res.render('join', { pageTitle: 'Join ' });
 }
 export const postJoin = async (req, res) => {
-    //create account
-    const { email, username, password, name, location } = req.body;
-    await User.create({ email, username, password, name, location });
+    const pageTitle = "Join";
+    const { email, username, password, password2, name, location } = req.body;
+    if (password !== password2) {
+        return res.render('join', { pageTitle, errorMessage: "입력하신 비밀번호가 일치하지 않습니다." });
+    }
+    const isNameOrEmailExist = await User.exists({ $or: [{ username }, { email }] });
+    if (isNameOrEmailExist) {
+        return res.render('join', { pageTitle, errorMessage: "이미 사용하고 있는 닉네임 또는 이메일 입니다." });
+    }
+    await User.create({ email, username, password, password2, name, location });
     return res.redirect('/login');
 }
 
