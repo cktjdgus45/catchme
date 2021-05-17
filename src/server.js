b9773e9fd9c1251a6d656938f957173d42b78be6
pooh <cktjdgus45@naver.com>
2021-05-17 19:44:09 +0900

    store session to mongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import session from 'express-session';
+import MongoStore from 'connect-mongo';
 import { localsMiddleware } from '../localsMiddleware';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
@@ -17,7 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(session({
     secret: "hello",
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/catchme' })
 }))
 
 app.use(localsMiddleware);
