06fd6573b8fe7d07cee26d1e5d4e6ab44fbdfd8b
pooh <cktjdgus45@naver.com>
2021-05-18 19:16:49 +0900

    github login finish

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/User';
 import bcrypt from 'bcrypt';
+import fetch from 'node-fetch';
 
 export const getLogin = (req, res) => {
     return res.render('login');
@@ -59,7 +60,7 @@ export const remove = (req, res) => {
 export const startGithubLogin = (req, res) => {
     const baseUrl = "https://github.com/login/oauth/authorize";
     const config = {
-        client_id: "dba39af3da1b27225f79",
+        client_id: process.env.GH_CLIENT,
         allow_signup: false,
         scope: "read:user user:email"
     }
@@ -68,6 +69,60 @@ export const startGithubLogin = (req, res) => {
     return res.redirect(finalUrl);
 }
 
-export const finishGithubLogin = (req, res) => {
+export const finishGithubLogin = async (req, res) => {
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code
+    }
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (await fetch(finalUrl, {
+        method: "POST",
+        headers: {
+            Accept: "application/json",
+        }
+    })).json();
+    if ("access_token" in tokenRequest) {
+        const { access_token } = tokenRequest;
+        const apiUrl = "https://api.github.com";
+        const userData = await (await fetch(`${apiUrl}/user`, {
+            headers: {
+                Authorization: `token ${access_token}`
+            }
+        })).json();
+        console.log(userData);
+        const emailData = await (await fetch(`${apiUrl}/user/emails`, {
+            headers: {
+                Authorization: `token ${access_token}`
+            }
+        })).json();
+        const emailObj = emailData.find(email => email.verified === true && email.primary === true);
+        if (!emailObj) {
+            return res.redirect("/login");
+        }
+        const existingUser = await User.findOne({ email: emailObj.email });
+        if (existingUser) {
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect('/');
 
+        } else {
+            //create an account
+            const user = await User.create({
+                email: emailObj.email,
+                username: userData.login,
+                password: "",
+                socialOnly: true,
+                name: userData.name,
+                location: userData.location
+            });
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect('/');
+        }
+    } else {
+        return res.redirect('/login');
+    }
 }
