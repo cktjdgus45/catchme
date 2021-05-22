43c3e8539b73b7c0d38cd4027e18a2aa33bec2aa
pooh <cktjdgus45@naver.com>
2021-05-23 08:45:38 +0900

    add protect route to  video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { protectorMiddleware } from '../../localsMiddleware';
 import { deleteVideo, getEdit, getUpload, postEdit, postUpload, watch } from '../controllers/videoController';
 
 const videoRouter = express.Router();
@@ -6,9 +7,9 @@ const videoRouter = express.Router();
 
 
 videoRouter.route('/:id([0-9a-f]{24})').get(watch);
-videoRouter.route('/:id([0-9a-f]{24})/delete').get(deleteVideo);
-videoRouter.route('/:id([0-9a-f]{24})/edit').get(getEdit).post(postEdit);
-videoRouter.route('/upload').get(getUpload).post(postUpload);
+videoRouter.route('/:id([0-9a-f]{24})/delete').all(protectorMiddleware).get(deleteVideo);
+videoRouter.route('/:id([0-9a-f]{24})/edit').all(protectorMiddleware).get(getEdit).post(postEdit);
+videoRouter.route('/upload').get(getUpload).all(protectorMiddleware).post(postUpload);
 
 
 export default videoRouter;
