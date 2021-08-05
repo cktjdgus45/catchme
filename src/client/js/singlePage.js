84c92bf5350d8c0bd503cdad78ae87cf703183e8
pooh <cktjdgus45@naver.com>
2021-08-05 18:38:43 +0900

    help

--- src/client/js/singlePage.js
+++ src/client/js/singlePage.js
@@ -1,46 +1,30 @@
 'use strict'
 
-const btn = document.querySelector('.btn');
-
 const positivePageHeight = window.innerHeight; //912
 const negativePageHeight = -window.innerHeight; //-912
 
-let ticking = false;
+const sections = document.querySelectorAll('section');
+console.log(sections);
 
-const doSomething = (y) => {
-    if (y < 0) {//y -100 , scroll up , top -
+const handleWheel = (e) => {
+    const scrolldirection = e.deltaY;
+
+    if (scrolldirection < 0) {//y -100 , scroll up , top -
         window.scrollBy({
             top: negativePageHeight,
             left: 0,
             behavior: 'smooth'
+            //up
         })
     } else {
         window.scrollBy({
             top: positivePageHeight,
             left: 0,
             behavior: 'smooth'
+            //down
         })
     }
+    window.removeEventListener('wheel', handleWheel);
 }
 
-window.addEventListener('wheel', function (e) {
-    const y = e.deltaY;
-    if (!ticking) {
-        window.requestAnimationFrame(function () {
-            doSomething(y);
-            ticking = false;
-        });
-        ticking = true;
-    }
-});
-btn.addEventListener('click', () => {
-    window.scrollBy({
-        top: positivePageHeight,
-        left: 0,
-        behavior: 'smooth'
-    })
-})
-
-
-
-
+window.addEventListener('wheel', handleWheel);
