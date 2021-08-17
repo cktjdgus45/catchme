af96422909469c81873b0de5e39ad07acb9326c8
pooh <cktjdgus45@naver.com>
2021-08-17 17:49:17 +0900

    handle mute btn event

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,7 @@ const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
 const time = document.getElementById('time');
-const volume = document.getElementById('volume');
+const volumeRange = document.getElementById('volume');
 
 //video play pause
 const handlePlayClick = (e) => {
@@ -13,17 +13,22 @@ const handlePlayClick = (e) => {
     } else {
         video.pause();
     }
+    playBtn.innerHTML = video.paused ? '<i class="fas fa-pause"></i>' : '<i class="fas fa-play"></i>';
 }
 
-const handlePlay = () => playBtn.innerHTML = '<i class="fas fa-pause"></i>';
-const handlePause = () => playBtn.innerHTML = '<i class="fas fa-play"></i>';
-
+//video muted unmuted
 const handleMute = (e) => {
+    if (video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtn.innerHTML = video.muted ? '<i class="fas fa-volume-mute"></i>' : '<i class="fas fa-volume-up"></i>';
+    volumeRange.value = video.muted ? 0 : 0.5;
 }
 
 playBtn.addEventListener('click', handlePlayClick);
-video.addEventListener('play', handlePlay)
-video.addEventListener('pause', handlePause);
+
 
 //video mute unmute
 muteBtn.addEventListener('click', handleMute);
