538e3df7aa944a06286fda77c0b7edaefe6b3d10
pooh <cktjdgus45@naver.com>
2021-08-24 16:45:01 +0900

    change form:description size

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: { type: String, required: true, trim: true, maxLength: 25 },
-    description: { type: String, required: true, trim: true, maxLength: 75 },
+    title: { type: String, required: true, trim: true, maxLength: 50 },
+    description: { type: String, required: true, trim: true },
     fileUrl: { type: String, required: true },
     createdAt: { type: Date, required: true, default: Date.now },
-    hashtags: [{ type: String, trim: true, maxLength: 25 }],
+    hashtags: [{ type: String, trim: true, maxLength: 50 }],
     comments: [{ type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" }],
     meta: {
         views: { type: Number, default: 0, required: true },
