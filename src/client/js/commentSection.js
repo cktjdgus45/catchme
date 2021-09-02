556029e24f792cd5f98c773351f716c3454a638e
pooh <cktjdgus45@naver.com>
2021-09-02 19:34:25 +0900

    delete // on commentSection

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,18 +1,18 @@
-const form = document.getElementById('commentForm');  //pug에 있는 html태그를 js에 가져옴.
-const delBtns = document.querySelectorAll('.delBtn'); ////pug에 있는 html태그를 js에 가져옴.
+const form = document.getElementById('commentForm');
+const delBtns = document.querySelectorAll('.delBtn');
 
 const addComment = (text, newCommentId, commentOwner, createdAt) => {
     const now = moment().format('YYYY-M-D-H-m-s'); //"현재시각"
     const nowArr = now.split('-');
-    const writeTime = moment(nowArr); //댓글쓴시간 ==현재
-    const currentTime = moment(createdAt); //현재시간
+    const writeTime = moment(nowArr);
+    const currentTime = moment(createdAt);
     let cmTime = writeTime.diff(currentTime, 'seconds');
     if (cmTime < 60) {
         cmTime = '방금 전';
     }
 
-    const videoComments = document.querySelector('.video__comments ul'); ////pug에 있는 html태그를 js에 가져옴.
-    const newComment = document.createElement("li");  //html을 js를 통해 새로만듬.
+    const videoComments = document.querySelector('.video__comments ul');
+    const newComment = document.createElement("li");
     const span2 = document.createElement("span");
     newComment.dataset.id = newCommentId;
     span2.innerText = '❌';
@@ -36,16 +36,16 @@ const addComment = (text, newCommentId, commentOwner, createdAt) => {
     `
     newComment.appendChild(span2);
     videoComments.prepend(newComment);
-    span2.addEventListener('click', handleDelete); // ❌ 가 클릭되면 handleDelete 실행.
+    span2.addEventListener('click', handleDelete);
 }
 
 const handleDelete = async (event) => {
     event.preventDefault();
-    const delbtn = event.target; // <span>❌</span> 와 동일.
-    const comment = delbtn.parentNode; // li 를 의미
-    const videoComments = comment.parentNode;  //ul 을 의미.
+    const delbtn = event.target;
+    const comment = delbtn.parentNode;
+    const videoComments = comment.parentNode;
     const commentId = comment.dataset.id;
-    const response = await fetch(`/api/comments/${commentId}`, { //누르면 우리의 서버로 delete 리퀘스트를 보냄. 보내서 서버에서 처리하면서 데이터베이스업데이트하고 다시 res받음.)
+    const response = await fetch(`/api/comments/${commentId}`, {
         method: "DELETE",
     })
     if (response.status === 201) {
@@ -55,14 +55,14 @@ const handleDelete = async (event) => {
 
 const handleSubmit = async (event) => {
     event.preventDefault();
-    const video = document.querySelector('video');////pug에 있는 html태그를 js에 가져옴.
-    const textarea = form.querySelector('textarea');////pug에 있는 html태그를 js에 가져옴.
+    const video = document.querySelector('video');
+    const textarea = form.querySelector('textarea');
     const videoId = video.dataset.id;
-    const text = textarea.value; //댓글 값.
+    const text = textarea.value;
     if (text === "") {
         return;
     }
-    const response = await fetch(`/api/videos/${videoId}/comment`, { //댓글 버튼을 누르면 우리서버로 POST 리퀘스트를 보냄 -> 서버는 요청을처리하면서 db업데이트하고 res 를 다시보냄.
+    const response = await fetch(`/api/videos/${videoId}/comment`, {
         method: "POST",
         body: JSON.stringify({ text }),
         headers: {
