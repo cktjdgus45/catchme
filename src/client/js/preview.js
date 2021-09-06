fbf761a12ffa32eb48d67f25f0b0d03d984444c0
pooh <cktjdgus45@naver.com>
2021-09-06 16:35:54 +0900

    init news branch

--- src/client/js/preview.js
+++ src/client/js/preview.js
@@ -10,36 +10,6 @@ function readImage(input) {
         reader.readAsDataURL(input.files[0])
     }
 }
-(function (document, url) {
-    const script = document.createElement("script");
-    script.src = url;
-    script.id = "canva-api"
-    script.onload = () => {
-        // API initialization
-        (async () => {
-            if (!window.Canva || !window.Canva.DesignButton) {
-                return;
-            }
-            const api = await window.Canva.DesignButton.initialize({
-                apiKey: "vTdFELO2OQBirLxwqyreI0EfqZcsBKnRtThGqFDAmWM=",
-            });
-            const button = document.querySelector("button");
-
-            button.onclick = () => {
-                api.createDesign({
-                    design: {
-                        type: "Poster",
-                    },
-                    onDesignOpen: (opts) => {
-                        console.log(opts);
-                    },
-                });
-            };
-        })();
-    };
-    document.body.appendChild(script);
-})(document, "https://sdk.canva.com/designbutton/v2/api.js");
-
 
 const inputImage = document.querySelector(".input-image")
 inputImage.addEventListener("change", e => {
