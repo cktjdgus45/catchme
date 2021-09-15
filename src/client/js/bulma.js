1b196af9069152663fcbea411684ad7b5a2c44d5
pooh <cktjdgus45@naver.com>
2021-09-15 14:11:45 +0900

    change header nav

--- src/client/js/bulma.js
+++ src/client/js/bulma.js
@@ -2,11 +2,19 @@ const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 const searchBtn = document.querySelector('.showSearchFrom');
 const searchForm = document.querySelector('.search__form');
+const userInfo = document.querySelector('.dropdown');
 
+//searchBtn - header
 searchBtn.addEventListener('click', () => {
     searchForm.classList.toggle('show');
 })
 
+//burgerIcon - reactive web
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
+})
+
+//user info - header
+userInfo.addEventListener('click', () => {
+    userInfo.classList.toggle('is-active');
 })
