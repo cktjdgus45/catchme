a3b37151250e3753300bbfdd5e638fee7b55f767
pooh <cktjdgus45@naver.com>
2021-09-23 12:29:54 +0900

    make question share scrren

--- src/client/js/videoRecord.js
+++ src/client/js/videoRecord.js
@@ -1,1 +1,11 @@
-console.log('hi')
+'use strict'
+
+const start = async () => {
+    const stream = await navigator.mediaDevices.getDisplayMedia({
+        video: {
+            mediaSource: "screen",
+        }
+    })
+}
+
+start();
