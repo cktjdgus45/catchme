c2939f28cba2ef9b88fde55b4ac401ed8d1138f7
pooh <cktjdgus45@naver.com>
2021-09-25 07:50:42 +0900

    merge audio,screen stream

--- src/client/js/videoRecord.js
+++ src/client/js/videoRecord.js
@@ -1,29 +1,58 @@
 const start = document.getElementById("start");
 const stop = document.getElementById("stop");
 const video = document.querySelector("video");
+
 let recorder, stream;
 
-async function startRecording() {
-    stream = await navigator.mediaDevices.getDisplayMedia({
-        video: { mediaSource: "screen" }
-    });
-    recorder = new MediaRecorder(stream);
+async function recordScreen() {
+    if (!(navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia)) {
+        return window.alert('Screen Record not supported!')
+    }
+    stream = null;
+    const displayStream = await navigator.mediaDevices.getDisplayMedia({ video: { cursor: "motion" }, audio: { 'echoCancellation': true } });
+    if (window.confirm("Record audio with screen?")) {
+        const audioContext = new AudioContext();
 
-    const chunks = [];
-    recorder.ondataavailable = e => chunks.push(e.data);
-    recorder.onstop = e => {
-        const completeBlob = new Blob(chunks, { type: chunks[0].type });
-        video.src = URL.createObjectURL(completeBlob);
+        const voiceStream = await navigator.mediaDevices.getUserMedia({ audio: { 'echoCancellation': true }, video: false });
+        const userAudio = audioContext.createMediaStreamSource(voiceStream);
+
+        const audioDestination = audioContext.createMediaStreamDestination();
+        userAudio.connect(audioDestination);
+
+        if (displayStream.getAudioTracks().length > 0) {
+            const displayAudio = audioContext.createMediaStreamSource(displayStream);
+            displayAudio.connect(audioDestination);
+        }
+
+        const tracks = [...displayStream.getVideoTracks(), ...audioDestination.stream.getTracks()]
+        stream = new MediaStream(tracks);
+        recorder = new MediaRecorder(stream);
+        const chunks = [];
+        recorder.ondataavailable = e => chunks.push(e.data);
+        recorder.onstop = e => {
+            const completeBlob = new Blob(chunks, { type: chunks[0].type });
+            video.src = URL.createObjectURL(completeBlob);
+        };
+
+        recorder.start();
+
+    } else {
+        stream = displayStream;
+        recorder = new MediaRecorder(stream);
+        const chunks = [];
+        recorder.ondataavailable = e => chunks.push(e.data);
+        recorder.onstop = e => {
+            const completeBlob = new Blob(chunks, { type: chunks[0].type });
+            video.src = URL.createObjectURL(completeBlob);
+        };
     };
-
-    recorder.start();
 }
 
 start.addEventListener("click", () => {
     start.setAttribute("disabled", true);
     stop.removeAttribute("disabled");
 
-    startRecording();
+    recordScreen();
 });
 
 stop.addEventListener("click", () => {
@@ -33,3 +62,4 @@ stop.addEventListener("click", () => {
     recorder.stop();
     stream.getVideoTracks()[0].stop();
 });
+
