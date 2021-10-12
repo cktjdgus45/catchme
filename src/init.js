0839aca929d7c04b9ecc88d6604fdb5439fb1b8a
pooh <cktjdgus45@naver.com>
2021-10-12 17:39:21 +0900

    build backend

--- src/init.js
+++ src/init.js
@@ -1,13 +1,10 @@
 import 'dotenv/config';
-
 import './db';
+import 'regenerator-runtime';
 import './models/Video';
 import './models/User';
 import './models/Comment';
-//DB 스키마와 DB 설정파일 불러오기
-
 import app from './server';
-//server설정 파일에서 app 가져오기
 
 const PORT = 4000;
 
@@ -16,5 +13,3 @@ const handleListening = () => {
 }
 
 app.listen(4000, handleListening);
-//서버 app 실행.
-//const app = express();
