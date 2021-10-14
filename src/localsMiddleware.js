6cfe3a41bbe9a88431169699d307aa908e301e0a
pooh <cktjdgus45@naver.com>
2021-10-14 22:13:29 +0900

    fix bug

--- src/localsMiddleware.js
+++ src/localsMiddleware.js
@@ -1,13 +1,13 @@
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
 const s3 = new aws.S3({
     credentials: {
         accessKeyId: process.env.AWS_ID,
-        secretAccessKey: process.env.AWS_SECRET
-    }
-})
+        secretAccessKey: process.env.AWS_SECRET,
+    },
+});
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -36,7 +36,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
 
 const multerUploader = multerS3({
     s3: s3,
-    bucket: 'catchme',
+    bucket: 'catchmebucket',
     acl: 'public-read',
 })
 
