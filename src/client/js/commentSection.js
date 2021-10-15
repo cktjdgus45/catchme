8902161a2b5c0774d26b3ed93a89058f7ba055f0
pooh <cktjdgus45@naver.com>
2021-10-15 21:55:56 +0900

    fix bug

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -8,18 +8,18 @@ const addComment = (text, newCommentId, commentOwner, createdAt) => {
     const commentTime = moment(createdAt);
     let cmTime = currentTime.diff(commentTime, 'seconds');
     if (cmTime < 60) {
-        return cmTime = currentTime.diff(commentTime, 'seconds') + '초전';
+        cmTime = currentTime.diff(commentTime, 'seconds') + '초전';
     }
     else if (cmTime > 60 && cmTime <= 3600) {
-        return cmTime = currentTime.diff(commentTime, 'minutes') + '분전';
+        cmTime = currentTime.diff(commentTime, 'minutes') + '분전';
     } else if (cmTime > 3600 && cmTime <= 86400) {
-        return cmTime = currentTime.diff(commentTime, 'hours') + '시간전';
+        cmTime = currentTime.diff(commentTime, 'hours') + '시간전';
     } else if (cmTime > 86400 && cmTime <= 2772000) {
-        return cmTime = currentTime.diff(commentTime, 'days') + '일전';
+        cmTime = currentTime.diff(commentTime, 'days') + '일전';
     } else if (cmTime > 2772000 && cmTime <= 31536000) {
-        return cmTime = currentTime.diff(commentTime, 'months') + '달전';
+        cmTime = currentTime.diff(commentTime, 'months') + '달전';
     } else if (cmTime > 31536000) {
-        return cmTime = currentTime.diff(commentTime, 'years') + '년전';
+        cmTime = currentTime.diff(commentTime, 'years') + '년전';
     }
 
     const videoComments = document.querySelector('.video__comments ul');
