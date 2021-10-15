9fa722eb53caebbaf1b1ded533b5e8d7a60fd5f6
pooh <cktjdgus45@naver.com>
2021-10-15 14:30:00 +0900

    add videoPlayer.js again

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -18,136 +18,135 @@ let controlsTimeout = null;
 let controlsMove = null;
 video.volume = volumeValue;
 
-//video muted unmute
-const handleMute = (e) => {
-    if (video.muted) {
-        video.muted = false;
-    } else {
-        video.muted = true;
-    }
-    muteBtn.innerHTML = video.muted ? '<i class="fas fa-volume-mute"></i>' : '<i class="fas fa-volume-up"></i>';
-    volumeRange.value = video.muted ? 0 : volumeValue;
-}
-muteBtn.addEventListener('click', handleMute);
+// //video muted unmute
+// const handleMute = (e) => {
+//     if (video.muted) {
+//         video.muted = false;
+//     } else {
+//         video.muted = true;
+//     }
+//     muteBtn.innerHTML = video.muted ? '<i class="fas fa-volume-mute"></i>' : '<i class="fas fa-volume-up"></i>';
+//     volumeRange.value = video.muted ? 0 : volumeValue;
+// }
+// muteBtn.addEventListener('click', handleMute);
 
-//video volume Range
-const handleVolumeChange = (event) => {
-    const { target: { value: volumeSize } } = event;
-    volumeValue = volumeSize;
-    video.volume = volumeSize;
-    if (video.muted) {
-        video.muted = false;
-        muteBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
-    }
-    muteBtn.innerHTML = volumeSize == 0 ? '<i class="fas fa-volume-off"></i>' : '<i class="fas fa-volume-up"></i>';
-}
-volumeRange.addEventListener('input', handleVolumeChange);
+// //video volume Range
+// const handleVolumeChange = (event) => {
+//     const { target: { value: volumeSize } } = event;
+//     volumeValue = volumeSize;
+//     video.volume = volumeSize;
+//     if (video.muted) {
+//         video.muted = false;
+//         muteBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+//     }
+//     muteBtn.innerHTML = volumeSize == 0 ? '<i class="fas fa-volume-off"></i>' : '<i class="fas fa-volume-up"></i>';
+// }
+// volumeRange.addEventListener('input', handleVolumeChange);
 
-//time duration
-const handleLoadedMetadata = () => {
-    currentTime.innerText = '00:00';
-    if (video.duration == Infinity) {
-        window.alert("지원되지 않는 파일입니다.")
-        return;
-    }
-    diff(video.duration)
-    timeLine.max = Math.floor(video.duration);
-}
-video.addEventListener('loadedmetadata', handleLoadedMetadata);
+// //time duration
+// const handleLoadedMetadata = () => {
+//     currentTime.innerText = '00:00';
+//     if (video.duration == Infinity) {
+//         window.alert("지원되지 않는 파일입니다.")
+//         return;
+//     }
+//     diff(video.duration)
+//     timeLine.max = Math.floor(video.duration);
+// }
+// video.addEventListener('loadedmetadata', handleLoadedMetadata);
 
-const handleFormatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5);
-const handleTimeUpdate = () => {
-    currentTime.innerText = handleFormatTime(Math.ceil(video.currentTime));
-    timeLine.value = Math.floor(video.currentTime);
-    if (video.ended) {
-        playBtn.innerHTML = '<i class="fas fa-play"></i>';
-    }
-}
+// const handleFormatTime = (seconds) => new Date(seconds * 1000).toISOString().substr(14, 5);
+// const handleTimeUpdate = () => {
+//     currentTime.innerText = handleFormatTime(Math.ceil(video.currentTime));
+//     timeLine.value = Math.floor(video.currentTime);
+//     if (video.ended) {
+//         playBtn.innerHTML = '<i class="fas fa-play"></i>';
+//     }
+// }
 
-const diff = (videoduration) => {
-    const front = Math.floor(videoduration / 60);
-    const back = Math.ceil(videoduration % 60);
-    totalTime.innerText = `${front}:${back}`;
-}
+// const diff = (videoduration) => {
+//     const front = Math.floor(videoduration / 60);
+//     const back = Math.ceil(videoduration % 60);
+//     totalTime.innerText = `${front}:${back}`;
+// }
 
-video.addEventListener('timeupdate', handleTimeUpdate);
+// video.addEventListener('timeupdate', handleTimeUpdate);
 
-//timeLine
-const handleTimeLineChange = (event) => {
-    const { target: { value } } = event;
-    video.currentTime = value;
-}
-timeLine.addEventListener('input', handleTimeLineChange);
+// //timeLine
+// const handleTimeLineChange = (event) => {
+//     const { target: { value } } = event;
+//     video.currentTime = value;
+// }
+// timeLine.addEventListener('input', handleTimeLineChange);
 
-//Full Screen
+// //Full Screen
 
-const handleFullScreen = () => {
-    const fullScreen = document.fullscreenElement;
-    if (fullScreen) {
-        document.exitFullscreen();
-        fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
-    } else {
-        videoContainer.requestFullscreen();
-        fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
-    }
-}
-fullScreenBtn.addEventListener('click', handleFullScreen);
+// const handleFullScreen = () => {
+//     const fullScreen = document.fullscreenElement;
+//     if (fullScreen) {
+//         document.exitFullscreen();
+//         fullScreenBtn.innerHTML = '<i class="fas fa-expand"></i>';
+//     } else {
+//         videoContainer.requestFullscreen();
+//         fullScreenBtn.innerHTML = '<i class="fas fa-compress"></i>';
+//     }
+// }
+// fullScreenBtn.addEventListener('click', handleFullScreen);
 
-//video controls event
-const hideControls = () => videoControls.classList.remove("showing");
+// //video controls event
+// const hideControls = () => videoControls.classList.remove("showing");
 
-const handleMouseMove = () => {
-    if (controlsTimeout) {
-        clearTimeout(controlsTimeout);
-        controlsTimeout = null;
-    }
-    if (controlsMove) {
-        clearTimeout(controlsMove);
-        controlsMove = null;
-    }
-    videoControls.classList.add("showing");
-    controlsMove = setTimeout(() => { hideControls() }, 2000);
-}
-const handleMouseLeave = () => {
-    controlsTimeout = setTimeout(() => {
-        hideControls();
-    }, 2000);
-}
+// const handleMouseMove = () => {
+//     if (controlsTimeout) {
+//         clearTimeout(controlsTimeout);
+//         controlsTimeout = null;
+//     }
+//     if (controlsMove) {
+//         clearTimeout(controlsMove);
+//         controlsMove = null;
+//     }
+//     videoControls.classList.add("showing");
+//     controlsMove = setTimeout(() => { hideControls() }, 2000);
+// }
+// const handleMouseLeave = () => {
+//     controlsTimeout = setTimeout(() => {
+//         hideControls();
+//     }, 2000);
+// }
 
-videoContainer.addEventListener('mousemove', handleMouseMove);
-videoContainer.addEventListener('mouseleave', handleMouseLeave);
-//play
-const handleVideoPlay = () => {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-    playBtn.innerHTML = video.paused ? '<i class="fas fa-play"></i>' : '<i class="fas fa-pause"></i>';
-}
-//play when click video
-video.addEventListener('click', (event) => {
-    handleVideoPlay();
-})
+// videoContainer.addEventListener('mousemove', handleMouseMove);
+// videoContainer.addEventListener('mouseleave', handleMouseLeave);
+// //play
+// const handleVideoPlay = () => {
+//     if (video.paused) {
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+//     playBtn.innerHTML = video.paused ? '<i class="fas fa-play"></i>' : '<i class="fas fa-pause"></i>';
+// }
+// //play when click video
+// video.addEventListener('click', (event) => {
+//     handleVideoPlay();
+// })
 
-//play when click playBtn
-const handlePlayClick = (e) => {
-    handleVideoPlay();
-}
-playBtn.addEventListener('click', handlePlayClick);
+// //play when click playBtn
+// const handlePlayClick = (e) => {
+//     handleVideoPlay();
+// }
+// playBtn.addEventListener('click', handlePlayClick);
 
-//play when press keyboard
-const handlePressKey = (e) => {
-    const pressedKey = e.code;
-    if (pressedKey === "Space" && e.target.localName !== "textarea") {
-        e.preventDefault();
-        handleVideoPlay();
-    }
-}
-document.addEventListener('keypress', handlePressKey);
+// //play when press keyboard
+// const handlePressKey = (e) => {
+//     const pressedKey = e.code;
+//     if (pressedKey === "Space" && e.target.localName !== "textarea") {
+//         e.preventDefault();
+//         handleVideoPlay();
+//     }
+// }
+// document.addEventListener('keypress', handlePressKey);
 
 //register view count
-
 const handleVideoEnded = async () => {
     const { dataset: { id } } = video;
     await fetch(`/api/videos/${id}/view`, {
